2ba9a49e460f6a9215a10763c6fb09c191da1d0d
Yehia Elbehery <yehia.elbehery@gmail.com>
2019-02-17 12:21:10 +0200

    Initial Commit

---
+++ AlamofireInterceptor/AlamofireInterceptor.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireInterceptor.h
+//  AlamofireInterceptor
+//
+//  Created by Nehal Elsawaf on 2/17/19.
+//  Copyright © 2019 Yehia Elbehery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireInterceptor.
+FOUNDATION_EXPORT double AlamofireInterceptorVersionNumber;
+
+//! Project version string for AlamofireInterceptor.
+FOUNDATION_EXPORT const unsigned char AlamofireInterceptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireInterceptor/PublicHeader.h>
+
+
